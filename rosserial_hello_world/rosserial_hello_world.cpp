be05f9f82e9fa2ca46a4cf2d7bdf7afe23f540a3
olindenaus <madorgon@gmail.com>
2020-04-25 17:29:50 +0200

    subscrive to ros messages

--- rosserial_hello_world/rosserial_hello_world.cpp
+++ rosserial_hello_world/rosserial_hello_world.cpp
@@ -23,12 +23,20 @@
 //
 #include <string>
 #include <stdio.h>
+#include <chrono>
 #include "ros.h"
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <tchar.h>
 #include <windows.h>
 
 using std::string;
+
+void estimated_pose_callback(const geometry_msgs::PoseWithCovarianceStamped& pose)
+ {
+       printf("Received pose %f, %f, %f\n", pose.pose.pose.position.x,
+                   pose.pose.pose.position.y, pose.pose.pose.position.z);
+ }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -38,23 +46,38 @@ int _tmain(int argc, _TCHAR* argv[])
     printf("Connecting to server at %s\n", ros_master);
     nh.initNode(ros_master);
 
+    /*SENDING*/
     printf("Advertising cmd_vel message\n");
     geometry_msgs::Twist twist_msg;
     ros::Publisher cmd_vel_pub("cmd_vel", &twist_msg);
     nh.advertise(cmd_vel_pub);
 
+    /*RECEIVING*/
+    ros::Subscriber < geometry_msgs::PoseWithCovarianceStamped > poseSub("estimated_pose", &estimated_pose_callback);
+    nh.subscribe(poseSub);
+
+    auto Start = std::chrono::high_resolution_clock::now();
     printf("Go robot go!\n");
+    int y = 0;
     while (1)
     {
+       /* y++;
+        auto End = std::chrono::high_resolution_clock::now();
         twist_msg.linear.x = 5.1;
-        twist_msg.linear.y = 0;
+        twist_msg.linear.y = y;
         twist_msg.linear.z = 0;
         twist_msg.angular.x = 0;
         twist_msg.angular.y = 0;
         twist_msg.angular.z = -1.8;
-        cmd_vel_pub.publish(&twist_msg);
-
+        cmd_vel_pub.publish(&twist_msg);*/
         nh.spinOnce();
+
+        //auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count();
+        //printf("%d\n", elapsed);
+        //
+        //if (elapsed >= 1000) {
+        //    break;
+        //}
         Sleep(100);
     }
 
